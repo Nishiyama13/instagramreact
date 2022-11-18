aa7115b46d2a6367a33f8bf48cea620994438f34
Nishiyama13 <nishiyamaeel@gmail.com>
2022-11-18 00:12:57 -0300

    Componente Post com props

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,79 +1,52 @@
 export default function Posts() {
+  const dataPost = [
+    { nomeUsuario: "meowed", imagemUsuario: "/assets/img/meowed.svg", imagemConteudo: "/assets/img/gato-telefone.svg", imagemLikeUsuario: "/assets/img/respondeai.svg", likeUsuario: "respondeai", likes: 101.523 },
+    { nomeUsuario: "barked", imagemUsuario: "/assets/img/barked.svg", imagemConteudo: "/assets/img/dog.svg", imagemLikeUsuario: "/assets/img/adorable_animals.svg", likeUsuario: "adorable_animals", likes: 99.159 }
+  ]
   return (
     <div class="posts">
-      <div class="post">
-        <div class="topo">
-          <div class="usuario">
-            <img src="/assets/img/meowed.svg" />
-            meowed
-          </div>
-          <div class="acoes">
-            <ion-icon name="ellipsis-horizontal"></ion-icon>
-          </div>
+      {dataPost.map((item) => <Post nomeUsuario={item.nomeUsuario} imagemUsuario={item.imagemUsuario} imagem={item.imagemConteudo} imagemLikeUsuario={item.imagemLikeUsuario} likeUsuarioName={item.likeUsuario} likes={item.likes} />)}
+    </div>
+  );
+}
+
+function Post(props) {
+  return (
+    <div class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={props.imagemUsuario} />
+          {props.nomeUsuario}
         </div>
-
-        <div class="conteudo">
-          <img src="/assets/img/gato-telefone.svg" />
-        </div>
-
-        <div class="fundo">
-          <div class="acoes">
-            <div>
-              <ion-icon name="heart-outline"></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-            <div>
-              <ion-icon name="bookmark-outline"></ion-icon>
-            </div>
-          </div>
-
-          <div class="curtidas">
-            <img src="/assets/img/respondeai.svg" />
-            <div class="texto">
-              Curtido por <strong>respondeai</strong> e{" "}
-              <strong>outras 101.523 pessoas</strong>
-            </div>
-          </div>
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
         </div>
       </div>
 
-      <div class="post">
-        <div class="topo">
-          <div class="usuario">
-            <img src="/assets/img/barked.svg" />
-            barked
+      <div class="conteudo">
+        <img src={props.imagem} />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
-          <div class="acoes">
-            <ion-icon name="ellipsis-horizontal"></ion-icon>
+          <div>
+            <ion-icon name="bookmark-outline"></ion-icon>
           </div>
         </div>
 
-        <div class="conteudo">
-          <img src="/assets/img/dog.svg" />
-        </div>
-
-        <div class="fundo">
-          <div class="acoes">
-            <div>
-              <ion-icon name="heart-outline"></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-            <div>
-              <ion-icon name="bookmark-outline"></ion-icon>
-            </div>
-          </div>
-
-          <div class="curtidas">
-            <img src="/assets/img/adorable_animals.svg" />
-            <div class="texto">
-              Curtido por <strong>adorable_animals</strong> e{" "}
-              <strong>outras 99.159 pessoas</strong>
-            </div>
+        <div class="curtidas">
+          <img src={props.imagemLikeUsuario} />
+          <div class="texto">
+            Curtido por <strong>{props.likeUsuarioName}</strong> e{" "}
+            <strong>outras {props.likes} pessoas</strong>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
